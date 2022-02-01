704007e2a1a4f3a92ef5d0ecd87714d15bcaf002
tolmachevroman <tolmachevroman@gmail.com>
2022-02-01 12:42:59 -0300

    add hardhat-abi-exporter

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("hardhat-abi-exporter");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -21,5 +22,11 @@ module.exports = {
         accountsBalance: "100000000000000000000", // 100 eth for each account
       },
     },
+  },
+  abiExporter: {
+    path: "./data/abi",
+    runOnCompile: true,
+    clear: true,
+    pretty: false,
   },
 };
